d9e97e5056bebadd2019d2cf3d4eba7bc0f73ea5
Mehdi <mbourouih94@gmail.com>
2022-12-07 10:42:42 +0100

    Update admin-panel.php

--- php/views/pages/admin-panel.php
+++ php/views/pages/admin-panel.php
@@ -2,7 +2,6 @@
 $pageTitle = "Admin panel - Mon site.com";
 
 ob_start();
-?>
 
 if (isset($_COOKIE['id_temporaly_admin']) && !empty($_COOKIE['id_temporaly_admin'])) {
     $id_temp = htmlspecialchars($_COOKIE['id_temporaly_admin']);
